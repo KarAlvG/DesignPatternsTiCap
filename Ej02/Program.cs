6d59b54a080bec0af9f1e5e574632f95a0af549b
KarAlvG <karalvarezg24@gmail.com>
2025-08-30 10:01:41 +0200

    Initial commit facade pattern Ej02

---
+++ Ej02/Program.cs
@@ -0,0 +1,33 @@
+﻿class Engine
+{
+    public void Start() => Console.WriteLine("Engine started");
+}
+
+class Lights
+{
+    public void TurnOn() => Console.WriteLine("Lights turned on");
+}
+
+class CarFacade
+{
+    private Engine Engine = new Engine();
+    private Lights Lights = new Lights();
+
+    public void StartCar()
+    {
+        Engine.Start();
+        Lights.TurnOn();
+        Console.WriteLine("Car is ready to go!");
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        var Car = new CarFacade();
+        Car.StartCar();
+    }
+}
